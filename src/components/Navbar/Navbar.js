059bbd0135740b57f8067f39da15fe09cdc896d6
Jordan M <jordan.muliaman@gmail.com>
2020-06-22 15:32:02 -0700

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Navbar = props => {
-
+const Navbar = () => {
+  return(
+    <nav className="navbar navbar-light bg-dark justify-content-center">
+      <a className="navbar-brand text-white" href="google.com">Employee Directory</a>
+    </nav>
+  )
 }
 
 export default Navbar
