be73d3c05af9d7dbf2c45b35a770abc10839a53e
Jordan M <jordan.muliaman@gmail.com>
2020-06-21 22:53:31 -0700

    create basic layout

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import Search from './components/Search'
+import Card from './components/Card'
+
+class App extends Component {
+
+}
+
+export default App
+
