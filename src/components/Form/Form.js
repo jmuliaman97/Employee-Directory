190249af31b9bcaba1dc8da683fce05fca287630
Jordan M <jordan.muliaman@gmail.com>
2020-06-22 20:32:12 -0700

    clean up form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,32 +3,30 @@ import React from 'react'
 const Form = props => {
   return (
     <form>
-      <div className="form-group">
-        <label>Categories</label>
-        <select
-            className="form-control"
-            onChange={props.handleCategory}>
-          <option>Gender</option>
-          <option>Country</option>
-          <option>Department</option>
-        </select>
-        <label htmlFor="input">Search</label>
-        <input
-            className="form-control"
-            type="text"
-            name="input"
-            id="name"
-            onChange={props.handleInputChange}
-            value={props.input}
-        />
-        <button
-          type="button"
-          className="btn btn-info"
-          onClick={props.handleSubmit}
-        >
-        Submit
-        </button>
-      </div>
+      <label>Categories</label>
+      <select
+        className="form-control"
+        onChange={props.handleCategory}>
+        <option>Gender</option>
+        <option>Country</option>
+        <option>Department</option>
+      </select>
+      <label>Search</label>
+      <input
+        className="form-control"
+        type="text"
+        name="input"
+        id="name"
+        onChange={props.handleInputChange}
+        value={props.input}
+      />
+      <button
+        type="button"
+        className="btn btn-info"
+        onClick={props.handleSubmit}
+      >
+      Submit
+      </button>
     </form>
   )
 }
