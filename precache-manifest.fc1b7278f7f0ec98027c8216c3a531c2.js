2de022f5b4995ef425f33a0f566d54f55afc5158
Jordan M <jordan.muliaman@gmail.com>
2020-06-22 21:03:48 -0700

    Updates

---
+++ precache-manifest.fc1b7278f7f0ec98027c8216c3a531c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b1a49cea91dcdd562643763b1d20f1",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "e13b3b5dad1fa7cc3d39",
+    "url": "/Employee-Directory/static/css/main.acbb27c9.chunk.css"
+  },
+  {
+    "revision": "ee7ae60c61dce23b33e5",
+    "url": "/Employee-Directory/static/js/2.beff07f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.beff07f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13b3b5dad1fa7cc3d39",
+    "url": "/Employee-Directory/static/js/main.48168f3b.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
